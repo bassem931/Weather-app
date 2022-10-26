9202b484d2be276c938a569f65b65a01fe20b0b2
bassem931 <bti.bassem@gmail.com>
2022-10-26 15:07:23 +0200

    improvments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-const WeatherData =[];
+const WeatherData = [];
 
 // Express to run server and routes
 const express = require('express');
@@ -16,7 +16,7 @@ const app = express();
 //Here we are configuring express to use body-parser as middle-ware.
 //app.use(bodyParser.urlencoded({extended:false}));
 //app.use(bodyParser.json());
-app.use(express.urlencoded({extended:false})); 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Cors for cross origin allowance
@@ -27,14 +27,14 @@ app.use(cors());
 app.use(express.static('website'));
 
 const port = 8000;
-const server = app.listen(port,()=>{
+const server = app.listen(port, () => {
     console.log("server running");
     console.log('server running at port ' + port);
 })
 
 
 // Callback function to complete GET '/all'
-app.get('/cities',(req, res) =>{
+app.get('/cities', (req, res) => {
     let NewCities = [];
     citiesArr.forEach(city => {
         NewCities.push(city.name);
@@ -42,43 +42,43 @@ app.get('/cities',(req, res) =>{
     res.send(NewCities);
 })
 
-app.get('/all',(req, res) =>{
+app.get('/all', (req, res) => {
     res.send(WeatherData);
 })
 
 //callback for weather
-app.get('/weather',(req, res) =>{
+app.get('/weather', (req, res) => {
     res.send(projectData);
 })
 
 
 // Post Route
-app.post('/addWeather',(req, res)=>{
+app.post('/addWeather', (req, res) => {
 
-newEntry = {
-    temp:req.body.temp,
-    date:req.body.date,
-    feeling:req.body.feeling,
-}
-WeatherData.push(newEntry);
-Object.assign(projectData,newEntry);
+    newEntry = {
+        temp: req.body.temp,
+        date: req.body.date,
+        feeling: req.body.feeling,
+    }
+    WeatherData.push(newEntry);
+    Object.assign(projectData, newEntry);
 })
 
 //post route advanced
-app.post('/addWeatherAdv',(req, res)=>{
-    
+app.post('/addWeatherAdv', (req, res) => {
+
     newEntry = {
-        weather:req.body.weather,
-        weatherIcon:req.body.weatherIcon,
-        humidity:req.body.humidity,
-        windSpeed:req.body.windSpeed,
-        country:req.body.country,
-        city:req.body.city,
-        temp:req.body.temp,
-        date:req.body.date,
-        feeling:req.body.feeling,
-        pressure:req.body.pressure,
+        weather: req.body.weather,
+        weatherIcon: req.body.weatherIcon,
+        humidity: req.body.humidity,
+        windSpeed: req.body.windSpeed,
+        country: req.body.country,
+        city: req.body.city,
+        temp: req.body.temp,
+        date: req.body.date,
+        feeling: req.body.feeling,
+        pressure: req.body.pressure,
     }
     WeatherData.push(newEntry);
-    Object.assign(projectData,newEntry);
-    })
+    Object.assign(projectData, newEntry);
+})
